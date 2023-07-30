9805659d54310357551382a05e4a860c2c750e51
Sharif <georginasharif@gmail.com>
2023-07-30 20:40:03 +0300

    feat: list available files and folder

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 #!/usr/bin/env node
-console.log("hello!");
+// import path from 'path';
+import fs from 'fs';
+// import { fileURLToPath } from 'url';
+
+// const __dirname = fileURLToPath(import.meta.url);
+// console.log(__dirname)
+// const dirPath = path.join(__dirname, '.');
+
+fs.readdir("./", (err, files) => {
+  if (err) {
+    console.log('unable to scan directory');
+    return;
+  }
+  files.forEach((file) => {
+    console.log(file);
+  });
+});
+
